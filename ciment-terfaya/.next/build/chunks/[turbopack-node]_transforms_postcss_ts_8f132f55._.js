8a4912d8a54bcc29a91d07c2e2be96a01f4e8b08
imadeddinebahou-lab <imadeddinebahou@gmail.com>
2026-03-12 10:51:55 +0100

    Add files via upload

---
+++ ciment-terfaya/.next/build/chunks/[turbopack-node]_transforms_postcss_ts_8f132f55._.js
@@ -0,0 +1,13 @@
+module.exports = [
+"[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/CIMENT TERFAYA/ciment-terfaya/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript, async loader)", ((__turbopack_context__) => {
+
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "chunks/fe56b_75358548._.js",
+  "chunks/[root-of-the-server]__038d36cb._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[turbopack-node]/transforms/postcss.ts { CONFIG => \"[project]/CIMENT TERFAYA/ciment-terfaya/postcss.config.mjs [postcss] (ecmascript)\" } [postcss] (ecmascript)");
+    });
+});
+}),
+];
